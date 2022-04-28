5790014ec06742c467f62b2e73b911eadad30794
yangyang121 <844171995@qq.com>
2022-04-29 07:59:55 +0800

    feat: 引入 antd-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+import { Button, message } from "ant-design-vue"
+Vue.use(Button)
 
+Vue.prototype.$message = message
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
